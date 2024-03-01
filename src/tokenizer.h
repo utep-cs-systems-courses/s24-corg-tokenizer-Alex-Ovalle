031cc7803a968fe6f54c001508d94be20ccdbef0
Alex-Ovalle <alexovalle3911@gmail.com>
2024-02-29 17:33:16 -0700

    hello

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,25 +5,65 @@
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
-
+int space_char(char c)
+{
+  if (c == '\t' || c == ' ') 
+  {
+    return 1;
+  }
+  return 0;
+}
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c)
+{
+  if (space_char(c))
+  {
+    return 0;
+  }
+  return 1;
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated token in zero-terminated str.  Return a zero pointer if 
    str does not contain any tokens. */
-char *token_start(char *str); 
+char *token_start(char *str)
+{
+  while(*str != '\0' && space_char(*str))
+  {
+    str++;
+  }
+  if(*str == '\0')
+  {
+    return 0;
+  }
+  return str;
+}
 
 /* Returns a pointer terminator char following *token */
-char *token_terminator(char *token);
+char *token_terminator(char *token)
+{
+  while (*token != '\0' && non_space_char(*token))
+  {
+    token++;
+  }
+  return token;
+}
 
 /* Counts the number of tokens in the string argument. */
-int count_tokens(char *str);
+int count_tokens(char *str)
+{
+  counter = 0;
+  while(*token_start(*str) != 0)
+  {
+    str = *token_terminator(*str);
+    counter++;
+  }
+  return counter;
+}
 
-/* Returns a fresly allocated new zero-terminated string 
+/* Returns a freshly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
 char *copy_str(char *inStr, short len);
 
